cdb2d12dd4fc9cc851b8a0034f25e70b06b4e8f7
BlaiseMUBADI <blaisemubadibakajika@gmail.com>
2025-09-24 21:43:53 +0100

    Initial commit du projet Gestion_Charroi_Auto

---
+++ src/nexus_bmb_soft/menu/MenuAction.java
@@ -0,0 +1,18 @@
+package nexus_bmb_soft.menu;
+
+/**
+ *
+ * @author Raven
+ */
+public class MenuAction {
+
+    protected boolean isCancel() {
+        return cancel;
+    }
+
+    public void cancel() {
+        this.cancel = true;
+    }
+
+    private boolean cancel = false;
+}
---
+++ src/nexus_bmb_soft/menu/MenuEvent.java
@@ -0,0 +1,10 @@
+package nexus_bmb_soft.menu;
+
+/**
+ *
+ * @author Raven
+ */
+public interface MenuEvent {
+
+    public void menuSelected(int index, int subIndex, MenuAction action);
+}
